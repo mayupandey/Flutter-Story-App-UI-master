13e9295d10d8b50a98125b07c6fac665d2598aaa
Microsoft <mayupandey1999@gmail.com>
2019-07-31 02:42:02 +0530

    Initial commit

---
+++ lib/data.dart
@@ -0,0 +1,15 @@
+List<String> images = [
+  "assets/image_04.jpg",
+  "assets/image_03.jpg",
+  "assets/image_02.jpg",
+  "assets/image_01.png",
+  "assets/image_02.jpg",
+];
+
+List<String> title = [
+  "ONE PIECE",
+  "POKEMON",
+  "CROP CRAFT",
+  "MONSTER MADOUSHI",
+  "BLACK CLOVER TV",
+];
